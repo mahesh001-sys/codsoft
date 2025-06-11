dd9b6034fd28734eba1d4ba97e13ea41c448ce0f
Mahesh <maheshbanoth057@gmail.com>
2025-06-11 12:40:01 +0530

    Update AtmInterface.java

--- AtmInterface.java
+++ AtmInterface.java
@@ -97,7 +97,7 @@ class BankAccount {
 }
 public class AtmInterface{
     public static void main(String[] args) {
-        BankAccount account = new BankAccount(10000); // Initial balance of 10,000
+        BankAccount account = new BankAccount(10000); 
         ATM atm = new ATM(account);
         atm.start();
     }
